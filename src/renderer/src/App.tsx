45b78f9e1047d0bc65670908300ae37c2fed2f64
needhourger <chainyuuki@gmail.com>
2025-08-26 15:49:34 +0800

    feat: add Material UI

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,10 +1,14 @@
+import Button from '@mui/material/Button'
+
 function App(): React.JSX.Element {
   const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
 
   return (
     <div className="bg-[url('/assets/wavy-lines.svg')]">
       <div className="text-2xl font-bold text-green-400">test tailwindcss</div>
-      <button onClick={ipcHandle}>Ping</button>
+      <Button variant="contained" onClick={ipcHandle}>
+        Hello world ping
+      </Button>
     </div>
   )
 }
