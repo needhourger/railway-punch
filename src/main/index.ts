28b03aaef8878a90bc01a7db2e9d9d12246b3b4e
needhourger <chainyuuki@gmail.com>
2025-09-01 18:15:27 +0800

    feat: change window size

--- src/main/index.ts
+++ src/main/index.ts
@@ -15,8 +15,8 @@ conf.registerRendererListener()
 function createWindow(): void {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 900,
-    height: 670,
+    width: 1280,
+    height: 720,
     show: false,
     autoHideMenuBar: true,
     ...(process.platform === 'linux' ? { icon } : {}),
